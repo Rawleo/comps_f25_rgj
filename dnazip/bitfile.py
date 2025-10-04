92d48cfcb8b33c5497894149b67986b78ea33f87
arroyoruizj <arroyoruizj@carleton.edu>
2025-10-04 14:50:16 -0500

    created function to turn positions into VINTs

---
+++ dnazip/bitfile.py
@@ -0,0 +1,39 @@
+def writeBItVINT(num):
+
+    bits = ''
+
+    while (num >= 128):
+
+        chrByte = ((num & 0x7F) | 0x80)
+        mask = 128
+
+        for i in range(7):
+
+            if (chrByte & mask):
+                bits.append(1)
+            else:
+                bits.append(0)
+
+            mask = mask >> 1
+
+        num = num >> 7
+    
+    chrByte = (num & 0x7F)
+    mask = 128
+
+    for i in range(7):
+
+        if (chrByte & mask):
+            bits.append(1)
+        else:
+            bits.append(0)
+
+        mask = mask >> 1
+    
+    return bits
+
+def main():
+
+    print(writeBItVINT(111))
+    print(writeBItVINT(655))
+    print(writeBItVINT(3))
