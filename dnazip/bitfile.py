0912e18afcdb613381e1976beee0fe66f59d8964
arroyoruizj <arroyoruizj@carleton.edu>
2025-10-06 12:20:43 -0500

    added bitfile.py back

---
+++ dnazip/bitfile.py
@@ -0,0 +1,39 @@
+def writeBItVINT(num):
+
+    bits = ''
+
+    while (num >= 128):
+
+        chrByte = ((num & 0x7F) | 0x80)
+        mask = 128
+
+        for i in range(8):
+
+            if (chrByte & mask):
+                bits += '1'
+            else:
+                bits += '0'
+
+            mask = mask >> 1
+
+        num = num >> 7
+
+    chrByte = num
+    mask = 128
+
+    for i in range(8):
+
+        if (chrByte & mask):
+            bits += '1'
+        else:
+            bits += '0'
+
+        mask = mask >> 1
+    
+    return bits
+
+def main():
+
+ print(writeBItVINT(300))
+
+main()
