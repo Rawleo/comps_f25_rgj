fd7ebc2d1e08ad12564f9cacfe17dfa932096f4e
arroyoruizj <arroyoruizj@carleton.edu>
2025-10-08 08:51:13 -0500

    Added function to turn VINTS into bytes and bytes back to VINTS

--- dnazip/bitfile.py
+++ dnazip/bitfile.py
@@ -1,6 +1,6 @@
-def writeBItVINT(num):
+def writeBitVINT(num):
 
-    bits = ''
+    bit_string = ''
 
     while (num >= 128):
 
@@ -10,9 +10,9 @@ def writeBItVINT(num):
         for i in range(8):
 
             if (chrByte & mask):
-                bits += '1'
+                bit_string += '1'
             else:
-                bits += '0'
+                bit_string += '0'
 
             mask = mask >> 1
 
@@ -24,16 +24,50 @@ def writeBItVINT(num):
     for i in range(8):
 
         if (chrByte & mask):
-            bits += '1'
+            bit_string += '1'
         else:
-            bits += '0'
+            bit_string += '0'
 
         mask = mask >> 1
     
-    return bits
+    return bit_string
+
+def BitStringToBytes(bit_string):
+
+    return int(bit_string, 2).to_bytes(len(bit_string) // 8, 'big')
+
+def BytesToBitString(bytes_obj):
+
+    return ''.join(format(byte, '08b') for byte in bytes_obj)
+
+
+def readBitVINT(bytes_obj):
+
+    bit_string = BytesToBitString(bytes_obj)
+    position = 0
+
+    while bit_string[0] == "1":
+
+        #figure out some math
+
+        bit_string = bit_string[7:]
+
+    # do something for the last 8 bits
+    
+    return position
 
 def main():
 
- print(writeBItVINT(300))
+    VINT_string = writeBitVINT(300)
+    byte_obj = BitStringToBytes(VINT_string)
+    byte_decode = BytesToBitString(byte_obj)
+
+    print('Testing Position to VINT: \n')
+    print('Original Number: 300')
+    print(f'VINT Representation: {VINT_string}')
+    print(f'Binary Representation: {byte_obj}')
+    print(f'Binary Back to String: {byte_decode}')
+
+    readBitVINT(byte_obj)
 
 main()
