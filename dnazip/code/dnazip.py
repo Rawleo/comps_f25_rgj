3e659d3780806b66a8fb08561cc24cf7f3c6c7c0
Ryan Son <sonryan50@gmail.com>
2025-10-17 13:01:01 -0500

    encoding per line

--- dnazip/code/dnazip.py
+++ dnazip/code/dnazip.py
@@ -21,7 +21,7 @@ def encode_file(input_file_path, dbSNP_path, k_mer_size):
     # chr_list = variants_df['chr'].unique()
     
     # Testing one chromosome (smallest one by BPs)
-    chr_list = ['chr21']
+    chr_list = ['chr19']
 
     for chr in chr_list:
         
@@ -38,19 +38,25 @@ def encode_file(input_file_path, dbSNP_path, k_mer_size):
         
         # Bitstring encoding of: chr#
         ascii_chr_bitstring = bitfile.encodeStringToBytes(chr)
-        print(ascii_chr_bitstring)
+        bitfile.export_as_binary(OUTPUT_PATH, ascii_chr_bitstring)
+        # print(ascii_chr_bitstring)
         
         # Start of SNPs
-        chr_encoding += ascii_chr_bitstring
-        chr_encoding += "00"
+        # chr_encoding += ascii_chr_bitstring
+        
         ## Add the number 0 for SNPs
+        # chr_encoding += "00"
+        bitfile.export_as_binary(OUTPUT_PATH, "00")
 
         # Encoding of Mapped SNPs
         bitmap, bitmap_size_vint, unmapped_df = dbsnp.compares_dbsnp(snps_df, dbSNP_path, chr)
         
         ### Add above to chr_encoding
-        chr_encoding += bitmap_size_vint
-        chr_encoding += bitmap
+        # chr_encoding += bitmap_size_vint
+        # chr_encoding += bitmap
+        
+        bitfile.export_as_binary(OUTPUT_PATH, bitmap_size_vint)
+        bitfile.export_as_binary(OUTPUT_PATH, bitmap)
 
         # Encoding of Unmapped SNPs
         snp_size_vint, unmapped_pos_bitstr, unmapped_nuc_bitstr = snp.encode_SNPs(unmapped_df)
@@ -59,32 +65,56 @@ def encode_file(input_file_path, dbSNP_path, k_mer_size):
         chr_encoding += snp_size_vint
         chr_encoding += unmapped_pos_bitstr
         chr_encoding += unmapped_nuc_bitstr
+        
+        bitfile.export_as_binary(OUTPUT_PATH, snp_size_vint)
+        bitfile.export_as_binary(OUTPUT_PATH, unmapped_pos_bitstr)
+        bitfile.export_as_binary(OUTPUT_PATH, unmapped_nuc_bitstr)
+
 
         #bit alignemnts?!
-        bitfile.export_as_binary(OUTPUT_PATH, chr_encoding)
-        chr_encoding = "" 
+        # bitfile.export_as_binary(OUTPUT_PATH, chr_encoding)
+        # chr_encoding = "" 
+
+        
 
         # Start of DELs
-        chr_encoding += ascii_chr_bitstring
-        chr_encoding += "01"
+        # chr_encoding += ascii_chr_bitstring
+        
+        bitfile.export_as_binary(OUTPUT_PATH, ascii_chr_bitstring)
+        # print(ascii_chr_bitstring)
+
+
         ## Add the number 1 for DELs
+        # chr_encoding += "01"
+        
+        bitfile.export_as_binary(OUTPUT_PATH, "01")
+
 
         # Encoding of DELs
         del_size_vint, del_pos_bitstr, del_len_bitstr = dels.encode_dels(dels_df)
         
         ### Add above to chr_encoding
-        chr_encoding += del_size_vint
-        chr_encoding += del_pos_bitstr
-        chr_encoding += del_len_bitstr
+        # chr_encoding += del_size_vint
+        # chr_encoding += del_pos_bitstr
+        # chr_encoding += del_len_bitstr
+        
+        bitfile.export_as_binary(OUTPUT_PATH, del_size_vint)
+        bitfile.export_as_binary(OUTPUT_PATH, del_pos_bitstr)
+        bitfile.export_as_binary(OUTPUT_PATH, del_len_bitstr)
         
         #bit alignemnts?!
-        bitfile.export_as_binary(OUTPUT_PATH, chr_encoding)
-        chr_encoding = "" 
+        # bitfile.export_as_binary(OUTPUT_PATH, chr_encoding)
+        # chr_encoding = "" 
+        
         
         # Start of INSRs 
         chr_encoding += ascii_chr_bitstring
-        chr_encoding += "10"
+        bitfile.export_as_binary(OUTPUT_PATH, ascii_chr_bitstring)
+
+
         ## Add the number 2 for INS
+        bitfile.export_as_binary(OUTPUT_PATH, "10")
+        chr_encoding += "10"
         
         # Encoding of INSRs
         ins_size_vint, ins_pos_bitstr, ins_len_bitstr, ins_bitstr_len_vint, ins_seq_bitstr = insr.encode_ins(insr_df, k_mer_size)
@@ -96,11 +126,18 @@ def encode_file(input_file_path, dbSNP_path, k_mer_size):
         chr_encoding += ins_bitstr_len_vint
         chr_encoding += ins_seq_bitstr
         
+        bitfile.export_as_binary(OUTPUT_PATH, ins_size_vint)
+        bitfile.export_as_binary(OUTPUT_PATH, ins_pos_bitstr)
+        bitfile.export_as_binary(OUTPUT_PATH, ins_len_bitstr)
+        bitfile.export_as_binary(OUTPUT_PATH, ins_bitstr_len_vint)
+        bitfile.export_as_binary(OUTPUT_PATH, ins_seq_bitstr)
+        
+        
         #bit alignemnts?!
-        bitfile.export_as_binary(OUTPUT_PATH, chr_encoding)
-        chr_encoding = "" 
+        # bitfile.export_as_binary(OUTPUT_PATH, chr_encoding)
                 
         genome_bitstring += chr_encoding
+        
         
         
         
